a08c2b817763695f5be998fc95c099a985f63d32
hiwi-dci-student <hiwot.abdi@dci.education>
2023-03-13 17:42:22 +0100

    setup, cleanup, components+js&css file created, routes and route done about only left

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Categories from "./component/Categories/Categories";
+import Header from "./component/Header/Header";
+import Instruments from "./component/Instruments/Instruments";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element= {<App/>}/>
+        <Route path="/Categories" element={<Categories/>}/>
+        <Route path="/Instruments" element={<Instruments/>}/>
+        <Route path= "/About" />
+
+
+      
+      </Routes>
     </div>
   );
 }
---
+++ src/component/Header/Header.js
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <NavLink to = "/">Home</NavLink>
+        <NavLink to = "/Categories">Categories</NavLink>
+        <NavLink to = "/Instruments">Instruments</NavLink>
+        <NavLink to = "/About">About</NavLink>
+    </div>
+  )
+}
+
