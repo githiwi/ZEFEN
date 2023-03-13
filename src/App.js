69a9a195c27047c5dcb74ea2e9c8f10c2d9176ec
hiwi-dci-student <hiwot.abdi@dci.education>
2023-03-13 17:44:44 +0100

    about route ok now on app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./component/About/About";
 import Categories from "./component/Categories/Categories";
 import Header from "./component/Header/Header";
 import Instruments from "./component/Instruments/Instruments";
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element= {<App/>}/>
         <Route path="/Categories" element={<Categories/>}/>
         <Route path="/Instruments" element={<Instruments/>}/>
-        <Route path= "/About" />
+        <Route path= "/About" element={<About/>} />
 
 
       
